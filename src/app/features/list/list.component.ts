9ea3bc742ea04ff0f6588493d44a74e2101d8f84
Adroaldo <hygino@msn.com>
2024-08-31 12:00:53 -0300

    Adicionando navegação para criar um produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
